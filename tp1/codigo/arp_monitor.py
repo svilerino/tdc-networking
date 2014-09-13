d2d8c7e8dc97c470dffb5c2b57af8871928c59f1
Agustín Garassino <ajgarassino@gmail.com>
2014-09-13 14:53:48 -0300

    Agregado para que calcule entropia de los who-has

--- tp1/codigo/arp_monitor.py
+++ tp1/codigo/arp_monitor.py
@@ -9,10 +9,15 @@ from scapy.all import *
 
 
 class Package(object):
-    def __init__(self, source, destination):
+    def __init__(self, source, destination, operation):
         self.source = source
         self.destination = destination
         self.time = datetime.datetime.now()
+		
+        if operation == 1:
+            self.operation = 'who-has'
+        elif operation == 2:
+            self.operation = 'is-at'
 
     def as_edge(self):
         return self.source, self.destination
@@ -31,7 +36,7 @@ class Histogram(object):
         pylab.ylim(0, max(self.data.values()) + 1)
         pylab.savefig('%s.png' % self.name, bbox_inches='tight')
         pylab.close()
-
+        
 
 class PackageStatistics(object):
     instance = None
@@ -45,9 +50,13 @@ class PackageStatistics(object):
     def __init__(self):
         self.graph = pgv.AGraph(bgcolor='white', directed=True)
         self.data_dst = {}
+        self.data_dst_who_has = {}
         self.data_src = {}
+        self.data_src_who_has = {}
         self.entropy_list_dst = []
+        self.entropy_list_dst_who_has = []
         self.entropy_list_src = []
+        self.entropy_list_src_who_has = []
 
     def add_to_graph(self, package):
         try:
@@ -60,12 +69,33 @@ class PackageStatistics(object):
     def add_to_data(self, package):
         self.data_dst.setdefault(package.destination, []).append(package.source)
         self.data_src.setdefault(package.source, []).append(package.destination)
+        if package.operation == 'who-has':
+			self.data_dst_who_has.setdefault(package.destination, []).append(package.source)
+			self.data_src_who_has.setdefault(package.source, []).append(package.destination)
 
     def add_package(self, package):
         self.add_to_graph(package)
         self.add_to_data(package)
         self.get_entropy_dst()
         self.get_entropy_src()
+        self.get_entropy_dst_who_has()
+        self.get_entropy_src_who_has()
+        
+    def get_entropy_dst_who_has(self):
+        time = datetime.datetime.now()
+        total = sum([len(value) for value in self.data_dst_who_has.values()])
+        probabilities = dict([(key, len(value) / float(total)) for key, value in self.data_dst_who_has.iteritems()])
+        entropy = sum([-math.log(p, 2) * p for p in probabilities.values()])
+        self.entropy_list_dst_who_has.append({'entropy': entropy, 'probabilities': probabilities, 'time': time})
+        print probabilities
+
+    def get_entropy_src_who_has(self):
+        time = datetime.datetime.now()
+        total = sum([len(value) for value in self.data_src_who_has.values()])
+        probabilities = dict([(key, len(value) / float(total)) for key, value in self.data_src_who_has.iteritems()])
+        entropy = sum([-math.log(p, 2) * p for p in probabilities.values()])
+        self.entropy_list_src_who_has.append({'entropy': entropy, 'probabilities': probabilities, 'time': time})
+        print probabilities
 
     def get_entropy_dst(self):
         time = datetime.datetime.now()
@@ -104,6 +134,28 @@ class PackageStatistics(object):
         pylab.plot_date(x, y)
         pylab.savefig('entropy_src.png', bbox_inches='tight')
         pylab.close()
+        
+    def draw_entropy_dst_who_has(self):
+        x = matplotlib.dates.date2num([e['time'] for e in self.entropy_list_dst_who_has])
+        y = [e['entropy'] for e in self.entropy_list_dst_who_has]
+        pylab.figure()
+        fig, ax = pylab.subplots()
+        pylab.setp(pylab.xticks()[1], rotation=30, ha='right')
+        ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M'))
+        pylab.plot_date(x, y)
+        pylab.savefig('entropy_dst_who_has.png', bbox_inches='tight')
+        pylab.close()
+
+    def draw_entropy_src_who_has(self):
+        x = matplotlib.dates.date2num([e['time'] for e in self.entropy_list_src_who_has])
+        y = [e['entropy'] for e in self.entropy_list_src_who_has]
+        pylab.figure()
+        fig, ax = pylab.subplots()
+        pylab.setp(pylab.xticks()[1], rotation=30, ha='right')
+        ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%H:%M'))
+        pylab.plot_date(x, y)
+        pylab.savefig('entropy_src_who_has.png', bbox_inches='tight')
+        pylab.close()
 
     def draw_graph(self):
         self.graph.layout(prog='dot')
@@ -116,7 +168,9 @@ class PackageStatistics(object):
 
     def draw_entropy(self):
         self.draw_entropy_src()
+        self.draw_entropy_src_who_has()
         self.draw_entropy_dst()
+        self.draw_entropy_dst_who_has()
 
     def draw(self):
         self.draw_graph()
@@ -125,7 +179,7 @@ class PackageStatistics(object):
 
 
 def monitor_callback(pkt):
-    package = Package(source=pkt.psrc, destination=pkt.pdst)
+    package = Package(source=pkt.psrc, destination=pkt.pdst, operation=pkt.op)
     PackageStatistics.get_instance().add_package(package)
     PackageStatistics.get_instance().draw()
     print pkt.sprintf("from %ARP.psrc% to %ARP.pdst%")
