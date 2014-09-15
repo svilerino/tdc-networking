b6646a09d143e4feeca31bb9c41d660bb012c18c
svilerino <svilerino@gmail.com>
2014-09-14 22:11:10 -0300

    scriptizado el installer

---
+++ tp1/codigo/install.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+#correr programa con permisos de super usuario
+sudo apt-get install python-pygraph python-pygraphviz python-scapy
+sudo apt-get install python-numpy python-scipy python-matplotlib
