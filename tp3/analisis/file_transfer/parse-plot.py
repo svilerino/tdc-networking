7c92ff548868ec9d3941e2f9f94b5b6c6710a1b6
Martin Santos <martin.n.santos@gmail.com>
2014-11-12 11:13:27 -0300

    Parser

--- tp3/analisis/file_transfer/parse-plot.py
+++ tp3/analisis/file_transfer/parse-plot.py
@@ -9,6 +9,8 @@ class Data:
 	def __init__(self):
 		self.alfas = []
 		self.betas = []
+		self.rtos = []
+		self.rtts = []
 		self.rtosProm = []
 		self.rttsProm = []
 		self.delays = []
@@ -21,8 +23,8 @@ class Data:
 			file = open(filename, "r")
 			lines = file.readlines()
 
-			rtts = []
-			rtos = []
+			rttsTmp = []
+			rtosTmp = []
 
 			for line in lines:
 				line = line.split()
@@ -32,28 +34,60 @@ class Data:
 					alfa = float(line[2])
 					beta = float(line[3])
 				if len(line)==2:
-					rtts.append(float(line[0]))
-					rtos.append(float(line[1]))
+					rttsTmp.append(float(line[0]))
+					rtosTmp.append(float(line[1]))
 
 			self.alfas.append(alfa)
 			self.betas.append(beta)
-			self.rtosProm.append(sum(rtos)/len(rtos))
-			self.rttsProm.append(sum(rtts)/len(rtts))
+			self.rtosProm.append(sum(rtosTmp)/len(rtosTmp))
+			self.rttsProm.append(sum(rttsTmp)/len(rttsTmp))
 			self.delays.append(delay)
 			self.pErrors.append(pError)
-			rtts = []
-			rtos = []
+			self.rtts.append(rttsTmp)
+			self.rtos.append(rtosTmp)
+			rttsTmp = []
+			rtosTmp = []
+
+	def show():
+		print "Alphas: "
+		print self.alfas
+		print "-----------------"
+		print "Betas: "
+		print self.betas
+		print "-----------------"
+		print "Delays: "
+		print self.delays
+		print "-----------------"
+		print "pErros: "
+		print self.pErros
+		print "-----------------"
+		print "RTO prom: "
+		print self.rtosProm
+		print "-----------------"
+		print "RTT prom: "
+		print self.rttsProm
+		print "-----------------"
+		print "RTO's: "
+		print self.rtts
+		print "-----------------"
+		print "RTT's: "
+		print self.rtos
+
 
 def graph(data, ejex, ejey, ejez):
-	plt.xlim=([0,1])
 	fig = plt.figure()
 	ax = fig.add_subplot(111, projection='3d')
+#	ax.set_xlim([0,1])
+#	ax.set_ylim([0,1])
+	ax.set_zlim([600,1200])
 #	for c, z in zip(['r', 'g', 'b', 'y', 'r']*5, data.betas):
-	for c, z in zip(['r', 'g', 'b'], data.betas):
+	for c, z in zip(['r', 'g', 'b', 'y', 'r']*5, data.betas):
 
 	    xs = data.alfas
+#	    xs = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+
 	    ys = data.rtosProm
-	    ys = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+#	    ys = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
 	    # You can provide either a single color or an array. To demonstrate this,
 	    # the first bar of each set will be colored cyan.
@@ -74,11 +108,6 @@ if __name__ == '__main__':
 	ejey = "Beta"
 	ejez = "RTO"
 	#graph(d,ejex,ejey,ejez)
+	d.show()
 
-#	print d.alfas
-#	print d.betas
-#	print d.rtosProm
-#	print d.rttsProm
-
-	graph(d,ejex,ejey,ejez)
-		
+			
